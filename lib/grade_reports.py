4e6d75b6c4ecacb36d4fba675138b826a0e8007e
Stephen Csengo <stephencsengo@gmail.com>
2023-08-17 12:00:08 -0400

    Codealong complete

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
-if __name__ == '__main__':
-    pass
+
+def create_grade_report(student_grades):
+    with open("lib/reports/grade_report.txt", "w") as gr:
+        for grade in student_grades:
+            gr.write(grade + "\n")
+
+
+if __name__ == "__main__":
+    student_grades = []
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        grade = input("Student name, grade: ")
+    create_grade_report(student_grades)
